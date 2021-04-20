0b1adf6e4355af4c0c66626f7d6237684ec9a541
Zeph Grunschlag <zephaniah@zibby.com>
2021-04-20 10:23:13 -0500

    iterating through the DFA!!!!

--- data_structures/tests/test_zrie.py
+++ data_structures/tests/test_zrie.py
@@ -1,4 +1,5 @@
 from data_structures.zrie import Zrie, DFA
+from itertools import product
 
 
 def test_zrie_basic():
@@ -7,9 +8,53 @@ def test_zrie_basic():
     assert sorted(words) == zrie.radix_sort()
     assert "cog" in zrie
     assert "cot" not in zrie
+    for w in zrie:
+        assert w in words
+
+    words = ["apple", "cherry", "dragonfruit",  "banana"]
+    zrie = Zrie(words)
+    assert sorted(words) == list(zrie)
+
+    words = {w[:i] for w in words for i in range(len(w)+1)}
+    assert len(words) == 29
+    zrie = Zrie(words)
+    assert sorted(words) == list(zrie)
 
 
-# def test_dfa_basic():
-#     words = ["cog", "dog", "dot", "hot", "log", "lot"]
-#     dfa = DFA(words)
-#     mdfa = dfa.minimize()
+def test_dfa_basic():
+    evens_init = {
+        ("", False): [("0", "0")] + [(c, "odds") for c in "13579"] + [(c, "evens") for c in "2468"],
+        ("0", True):  [],
+        ("evens", True): [(c, "odds") for c in "13579"] + [(c, "evens") for c in "02468"],
+        ("odds", False): [(c, "odds") for c in "13579"] + [(c, "evens") for c in "02468"],
+    }
+    evens = DFA("", evens_init)
+    for i in range(100):
+        if i % 2 == 0:
+            assert str(i) in evens
+        else:
+            assert str(i) not in evens
+    assert "" not in evens
+    assert len(evens) == 4
+
+    for i, e in enumerate(evens):
+        print(e)
+        # assert 2*i == int(e)
+        if i > 10:
+            break
+
+    binary_evens_init = {
+        ("0", True): [('0', "0"), ('1', "1")],
+        ("1", False): [('0', "0"), ('1', "1")],
+    }
+    binary_evens = DFA("0", binary_evens_init)
+    for x in map("".join, product(*([["0", "1"]]*5))):
+        i = int(x, 2)
+        if i % 2 == 0:
+            assert x in binary_evens
+        else:
+            assert x not in binary_evens
+    assert "" not in evens
+    assert len(binary_evens) == 2
+
+    # mdfa = dfa.minimize()
--- data_structures/zrie.py
+++ data_structures/zrie.py
@@ -1,16 +1,17 @@
 from __future__ import annotations
-from typing import List, Optional, Dict, DefaultDict
-from collections import defaultdict
+from typing import List, Optional, Dict, DefaultDict, Iterable
+from collections import defaultdict, deque
 
 
 class State:
-    def __init__(self, prefix: str = "", accept: bool = False):
+    def __init__(self, prefix: str = "", accept: bool = False, verbose: bool = False):
         """
         In the FSA case, `prefix` is just a label and can be ignored
         """
         self.prefix, self.accept = prefix, accept
         self.transitions: DefaultDict()[str, State] = defaultdict(list)
         self.radix_sort_keys: Optional[List[str]] = None
+        self.verbose = verbose
 
     def add_transition(self, c: str, target: State):
         assert len(c) == 1, f"transitions should be alphabetical, but given c={c}"
@@ -19,11 +20,19 @@ class State:
     def set_radix_sortable(self):
         self.radix_sort_keys = sorted(self.transitions.keys())
 
-    def accepted(self, word: str):
+    def accepts(self, word: str):
+        if self.verbose:
+            print(f"{self.prefix}[{'accept' if self.accept else 'reject'}].accepts({word})?")
         if not word:
             return self.accept
         c = word[0]
-        return self.transitions[c].accepted(word[1:]) if c in self.transitions else False
+        return self.transitions[c].accepts(word[1:]) if c in self.transitions else False
+
+    def __iter__(self) -> Iterable:
+        yield self
+        for k in self.radix_sort_keys:
+            for s in self.transitions[k]:
+                yield s
 
 
 class Zrie:
@@ -67,8 +76,72 @@ class Zrie:
         return collected
 
     def __contains__(self, word: str) -> bool:
-        return self.start.accepted(word)
+        return self.start.accepts(word)
+
+    def __iter__(self) -> Iterable:
+        def preorder(prefix, state):
+            if state.accept:
+                yield prefix
+            for c in state.radix_sort_keys:
+                yield from preorder(prefix+c, state.transitions[c])
+
+        yield from preorder("", self.start)
 
 
 class DFA:
-    pass
+    def __init__(self, start: str, transitions: Dict[Tuple[str, bool], List[Tuple[str, str]]]):
+        self.states: Dict[str, State] = {}
+        self.start: State = None
+
+        for state_accept, translist in transitions.items():
+            state, accept = state_accept
+            if state not in self.states:
+                self.states[state] = State(prefix=state)
+            s = self.states[state]
+            s.accept = accept
+
+            if state == start:
+                self.start = s
+
+            for c, target in translist:
+                assert isinstance(c, str)
+                assert len(c) == 1
+                if target not in self.states:
+                    self.states[target] = State(prefix=target)
+                s.transitions[c] = self.states[target]
+
+        assert self.start is not None
+        for state in self.states.values():
+            state.set_radix_sortable()
+
+    def __len__(self) -> int:
+        return len(self.states)
+
+    def __contains__(self, word: str) -> bool:
+        return self.start.accepts(word)
+
+    def __iter__(self) -> Iterable:
+        """
+        Iterate by order of length and lexicographically
+        """
+        q = deque([("", self.start)])
+        while q:
+            prefix, state = q.popleft()
+            if state.accept:
+                yield prefix
+            for c in state.radix_sort_keys:
+                q.append((prefix + c, state.transitions[c]))
+
+    # def __iter__(self) -> Iterable:
+    #     visited = set()
+
+    #     def preorder(prefix, state):
+    #         visited.add(prefix)
+    #         print(visited)
+    #         if state.accept:
+    #             print(prefix)
+    #             yield prefix
+    #         for c in state.radix_sort_keys:
+    #             if prefix+c not in visited:
+    #                 yield from preorder(prefix + c, state.transitions[c])
+    #     yield from preorder("", self.start)
